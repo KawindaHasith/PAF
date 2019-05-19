c77a5b35bb77c6892f6590d12bfce344da7851a5
KawindaHasith <kawindaptr@gmail.com>
2019-05-19 20:13:02 +0530

    adasdad

---
+++ SellNBuy/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customers values(1,'addr1','email1','Kawinda','Jayasundara','pass1','0766867936','uname1');
+insert into customers values(2,'addr2','email2','Kawinda','Jayasundara','pass2','0766867936','uname2');
+insert into customers values(3,'addr3','email3','Kawinda','Jayasundara','pass3','0766867936','uname3');
